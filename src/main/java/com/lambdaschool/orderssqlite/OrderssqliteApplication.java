2be6185c1d3668c19272eab322feffa3686726ac
Danny Vail <danny_vail@outlook.com>
2019-02-20 18:34:14 -0500

    INITIAL COMMIT Setup Spring project

---
+++ src/main/java/com/lambdaschool/orderssqlite/OrderssqliteApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.orderssqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderssqliteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrderssqliteApplication.class, args);
+  }
+
+}
